c3c0912cecdbf5dfa6743d14f8e98de54fc50fc9
Adryan Gabriel <adryangabrielrodrigues13@gmail.com>
2026-01-02 13:55:39 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--caixa de imports e exports-->
+<!--define 'App.Vue' como "pai" e os imports de 'components como "filhos"-->
+<script lang="ts">
+
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+export default{
+  components: {Banner, ConteudoPrincipal}
+}
+
+</script>
+
+
+<template> 
+
+  <Banner/>
+  <ConteudoPrincipal/>
+
+</template>
